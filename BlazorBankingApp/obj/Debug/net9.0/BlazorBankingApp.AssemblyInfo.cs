db80f139bb51d74ebaf0f538bf638b4d6e3e3cf8
Zach <167152581+ZVogel1@users.noreply.github.com>
2025-03-17 14:38:40 -0400

    N/A

--- BlazorBankingApp/obj/Debug/net9.0/BlazorBankingApp.AssemblyInfo.cs
+++ BlazorBankingApp/obj/Debug/net9.0/BlazorBankingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorBankingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fe9cd4cdd29bec46b51082ef56464f5e9f1414a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1a15987d9d7132c586370bcc0344ec5887ab632")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorBankingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorBankingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
