467603ffb9d828a5d17c2162fab0fc0b72eb3e75
Zach <167152581+ZVogel1@users.noreply.github.com>
2025-03-19 17:53:05 -0400

    Finished the login logic
    Having issues going to home page and signup page

--- BlazorBankingApp/obj/Debug/net9.0/BlazorBankingApp.AssemblyInfo.cs
+++ BlazorBankingApp/obj/Debug/net9.0/BlazorBankingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorBankingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf977ffac43594c3430010bccb80a031f77f741e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab17aad1136b0f6ebb9bacb9fa584c6eecb39468")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorBankingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorBankingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
