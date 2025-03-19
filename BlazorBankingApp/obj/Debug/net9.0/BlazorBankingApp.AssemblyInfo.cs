ab17aad1136b0f6ebb9bacb9fa584c6eecb39468
Zach <167152581+ZVogel1@users.noreply.github.com>
2025-03-19 15:47:05 -0400

    Working on login code

--- BlazorBankingApp/obj/Debug/net9.0/BlazorBankingApp.AssemblyInfo.cs
+++ BlazorBankingApp/obj/Debug/net9.0/BlazorBankingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorBankingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ecffa8679ef83bc1fe07198a085fa60b2043ff9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf977ffac43594c3430010bccb80a031f77f741e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorBankingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorBankingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
