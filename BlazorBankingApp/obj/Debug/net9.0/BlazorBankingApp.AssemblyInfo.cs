6c4d07fa81cc7443d7cfa1d74c4902dc857ad636
Martin Anuonye <manuonye@charlotte.edu>
2025-03-24 15:30:48 -0400

    Edit to home

--- BlazorBankingApp/obj/Debug/net9.0/BlazorBankingApp.AssemblyInfo.cs
+++ BlazorBankingApp/obj/Debug/net9.0/BlazorBankingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorBankingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+771a813d9eb49e1fd4540954370e24c9bee73472")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09a11c42dc6dae9d9357e62c371558d8fa913a5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorBankingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorBankingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
