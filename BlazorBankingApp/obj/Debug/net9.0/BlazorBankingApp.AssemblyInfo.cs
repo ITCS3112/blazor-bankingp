ccf91fdc24e502ca0494bd742fa686e90e5c5f49
Zach <167152581+ZVogel1@users.noreply.github.com>
2025-03-30 09:16:00 -0400

    Reset password and Sign up
    Finished signup but having issues with reset password code

--- BlazorBankingApp/obj/Debug/net9.0/BlazorBankingApp.AssemblyInfo.cs
+++ BlazorBankingApp/obj/Debug/net9.0/BlazorBankingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorBankingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a91dc1aa6524241db315ad9e87e0e95b584887b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60fcfa6acb4e63f24c7c803c37363345e8f4f63e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorBankingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorBankingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
