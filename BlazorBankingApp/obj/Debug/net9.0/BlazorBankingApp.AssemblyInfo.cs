ddbc1ed2fcd8f6bd77644c9028e97b6bcb21f117
Zach <167152581+ZVogel1@users.noreply.github.com>
2025-04-07 15:43:49 -0400

    Worked on Balance fetching

--- BlazorBankingApp/obj/Debug/net9.0/BlazorBankingApp.AssemblyInfo.cs
+++ BlazorBankingApp/obj/Debug/net9.0/BlazorBankingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorBankingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6a91d4cde3a8487df357c1ee84f397e5b1cdf06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+030018e9cca2a14283216fa32f86a3fecbdf1782")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorBankingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorBankingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
