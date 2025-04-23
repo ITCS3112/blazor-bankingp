bbf5a2ad5a68724f04f6a15380328ec5634c8f20
JamesPulliam <160961482+JamesPulliam@users.noreply.github.com>
2025-04-23 12:42:08 -0400

    Added the MoneyManagement.razor into the program

--- BlazorBankingApp/obj/Debug/net9.0/BlazorBankingApp.AssemblyInfo.cs
+++ BlazorBankingApp/obj/Debug/net9.0/BlazorBankingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorBankingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a15c39e64467c94fa9e23507646bd0444103829c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f17ba365f5f365e718ea70c8f780ede4af3037fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorBankingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorBankingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
